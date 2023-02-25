05d96e44189170d1a39fb80f0010c3ebdf1ce74e
FuuKowatty <miedzio.2000@gmail.com>
2023-02-25 17:34:13 +0100

    eslint and prettier configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,29 +1,60 @@
-module.exports = 
-{
-    "env": {
-      "browser": true,
-      "es2021": true
-    },
-    "extends": [
-      "eslint:recommended",
-      "plugin:react/recommended",
-      "plugin:@typescript-eslint/recommended",
-      "plugin:react/jsx-runtime"
+module.exports = {
+  env: {
+    node: true,
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:import/warnings',
+    'prettier',
+  ],
+  overrides: [],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', '@typescript-eslint'],
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'warn',
+    'react/prop-types': 'off',
+    'import/prefer-default': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'prefer-const': 'error',
+    'react/function-component-definition': [
+      'error',
+      {
+        namedComponents: 'arrow-function',
+      },
     ],
-    "overrides": [],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-      "ecmaVersion": "latest",
-      "sourceType": "module"
+    'import/order': [
+      'warn',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'unknown',
+          'parent',
+          'sibling',
+          'index',
+        ],
+      },
+    ],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['src'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
     },
-    "plugins": ["react", "@typescript-eslint"],
-    "rules": {
-      "no-console": "warn",
-      "no-debugger": "warn",
-      "react/prop-types": "off",
-      "@typescript-eslint/explicit-module-boundary-types": "off",
-      "indent": ["error", 2],
-      "quotes": ["error", "single"],
-      "semi": ["error", "always"]
-    }
-  }
+  },
+};
