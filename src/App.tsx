0969267608686a11c75855bf5346eaf0db176219
FuuKowatty <miedzio.2000@gmail.com>
2023-02-26 16:25:20 +0100

    redux basic template

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
-import './App.css';
-
 const App = () => {
-  return <div className="App"></div>
+  return <div className="App">Hello World</div>;
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,15 @@
 import ReactDOM from 'react-dom/client';
+import { AppProviders } from 'appProviders/AppProviders';
 import App from './App';
-import './index.css';
+import '@fontsource/roboto';
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
-  <>
+  <AppProviders>
     <App />
-  </>,
+  </AppProviders>
+    
 );
