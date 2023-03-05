8bb1cd4f472cf7ff7a00761f8f37bc401c77a67b
FuuKowatty <miedzio.2000@gmail.com>
2023-03-05 16:03:00 +0100

    added promotion component

---
+++ src/components/Promotions/index.tsx
@@ -0,0 +1,54 @@
+import { useState, useEffect, useRef } from 'react';
+import { Box } from '@mui/system';
+import { Slide } from '@mui/material';
+import { MessageText, PromotionsContainer } from 'styles/Promotions/index';
+
+
+const messages = [
+  '20% off on your first order!',
+  'Summer sale starts now, visit any store.',
+  'Please like and subscribe :)',
+];
+
+export const Promotions = () => {
+  const [messageIndex, setMessageIndex] = useState(0);
+  const [show, setShow] = useState(true);
+  const containerRef = useRef();
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+
+    const intervalId = setInterval(() => {
+      setMessageIndex((i) => (i + 1) % messages.length);
+      setShow(true);
+
+      setTimeout(() => {
+        setShow(false);
+      }, 3000);
+    }, 4000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  return (
+    <PromotionsContainer ref={containerRef}>
+      <Slide
+        container={containerRef.current}
+        in={show}
+        direction={show ? 'right' : 'left'}
+        timeout={{
+          enter: 800,
+          exit: 100,
+        }}
+      >
+        <Box display="flex" justifyContent="center" alignItems="center">
+          <MessageText>{messages[messageIndex]}</MessageText>
+        </Box>
+      </Slide>
+    </PromotionsContainer>
+  );
+};
---
+++ src/styles/Promotions/index.ts
@@ -0,0 +1,25 @@
+import { styled } from '@mui/material/styles';
+import { Box, Typography } from '@mui/material';
+import { Colors } from '../theme';
+
+export const PromotionsContainer = styled(Box)(({theme}) => ({
+  [theme.breakpoints.up('md')]: {
+    padding: '40px 0',
+  },
+  marginTop: '10 px',
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'center',
+  padding: '20px 0px 20px 0px',
+  overflow: 'hidden',
+  background: Colors.dark_blue,
+}));
+
+export const MessageText = styled(Typography)(({ theme }) => ({
+  fontFamily: '"Montez", "cursive"',
+  [theme.breakpoints.up('md')]: {
+    fontSize: '3rem',
+  },
+  color: Colors.primary,
+  fontSize: '1.5rem',
+}));
