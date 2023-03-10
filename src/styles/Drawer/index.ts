d93a04550de679d28cc4ebb72db08708228ef23c
FuuKowatty <miedzio.2000@gmail.com>
2023-03-10 18:41:58 +0100

    drawer styled component template

---
+++ src/styles/Drawer/index.ts
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+import { Drawer, IconButton } from '@mui/material';
+import { Colors } from 'styles/theme';
+
+
+export const DrawerContainer = styled(Drawer)(() => ({
+
+}));
+
+export const DrawerIconButton = styled(IconButton)(() => ({
+    position: 'fixed',
+    top: 20,
+    left: 20,
+    color: Colors.primary,
+    zIndex: 1999
+}));
