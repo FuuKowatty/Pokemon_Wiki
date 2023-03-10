3820b2b341621860dca97d6176aea6ed3146f6bb
FuuKowatty <miedzio.2000@gmail.com>
2023-03-10 18:44:58 +0100

    added AppDrawer component

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import { useEffect } from 'react';
 import { Home } from 'pages/Home';
 import { Navbar } from 'components/Navbar/index';
 import { Detail } from 'pages/Detail';
+import { AppDrawer } from 'components/Drawer';
 
 const App = () => {
-
   useEffect(() => {
     document.title = 'POKEWIKI - Wikipedia of pokemons';
   }, []);
@@ -13,6 +13,7 @@ const App = () => {
   return (
     <BrowserRouter>
       <Navbar />
+      <AppDrawer />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/detail" element={<Detail />} />
